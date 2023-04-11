d919419ad7ea04b72a98f54377b794d8944ff776
manash7 <manashgupta420@gmail.com>
2023-04-11 10:41:39 +0530

    First Commit with All Required and initial Files

---
+++ MovieRentalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieRentalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
